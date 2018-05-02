5f504e0e67ca658956a45d7b5acd3e35c02f6961
Andre I <aisaac2020@bklawtech.com>
2018-05-02 22:18:34 +0000

    can now view json weather for zip code

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
 /* global $ */
 
-console.log('hi');
-
-$('h1').click(function(){
-    $('h1').css('color', 'green');
-    $('h1').text('Ready to Code');
+$('#submit').click(function() {
+    $.ajax({
+        method: "GET",
+        url: "https://api.openweathermap.org/data/2.5/weather?zip=" + $("#zipInput").val() + ",us&APPID=b63c21ba1c2132722cab7cd4298f807d",
+        success: function(data) {
+            $("#results").html(
+                "<div>" +
+                    "<img src=http://openweathermap.org/img/w/" + data.weather[0].icon + ".png />" + 
+                "</div>" +
+                "<div>" +
+                    "<pre>" + JSON.stringify(data,null,4) + "</pre>" +
+                "</div>"
+            )
+        }
+    }).then(function(){
+        $("#zipInput").val("");
+    })
 });
